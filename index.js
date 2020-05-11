ff8c66b98bf59db15f2ce241c38030531f98fcc2
Vitaliy Mikhaylyyk <vitaliymikhaylyuk@gmail.com>
2020-05-11 17:43:59 -0400

    delete endpoint

--- index.js
+++ index.js
@@ -26,7 +26,33 @@ server.post("/api/users", (req, res) => {
 })
 
 server.get("/api/users", (req, res) => {
-    res.json({message: "hello, world"})
+    if (users) {
+        res.status(200).json(users)
+    } else {
+        res.stauts(500).json({ errorMessage: "There was an error while saving the user to the database" })
+    }
+});
+
+server.get("/api/users/:id", (req, res) => {
+    
+});
+
+server.delete("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+
+    const found = users.find(user => user.id === id);
+    if (found) {
+        user = users.filter(user => user.id !== id);
+        res.status(200).json(found)
+    } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    }
+    //need to add: if there is error removing user from database, respond with (500 code), { errorMessage: "The user could not be removed" }
+});
+
+server.patch("/api/users/:id", (req, res) => {
+
 });
 
 server.listen(3000, () => {
